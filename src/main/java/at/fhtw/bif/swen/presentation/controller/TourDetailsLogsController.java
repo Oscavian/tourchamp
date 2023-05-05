88268fcc15f33807562dbfedde344acd9040b154
Oscavian <dev@oscavian.com>
2023-05-05 14:41:16 +0200

    Add log view & adjust route view

---
+++ src/main/java/at/fhtw/bif/swen/presentation/controller/TourDetailsLogsController.java
@@ -0,0 +1,32 @@
+package at.fhtw.bif.swen.presentation.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TourDetailsLogsController implements Initializable {
+    public Button addLogButton;
+    public Button deleteLogButton;
+    public Button clearAllLogsButton;
+    public VBox addLogForm;
+    public TableView logTable;
+
+    public void addLogEntry(ActionEvent actionEvent) {
+    }
+
+    public void deleteEntry(ActionEvent actionEvent) {
+    }
+
+    public void clearAllLogs(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
--- src/main/java/at/fhtw/bif/swen/presentation/controller/TourDetailsRouteController.java
+++ src/main/java/at/fhtw/bif/swen/presentation/controller/TourDetailsRouteController.java
@@ -1,4 +1,23 @@
 package at.fhtw.bif.swen.presentation.controller;
 
-public class TourDetailsRouteController {
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TourDetailsRouteController implements Initializable {
+    public ImageView routeMap;
+
+    private Image placeholderImage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        placeholderImage = new Image("https://images.unsplash.com/photo-1475359524104-d101d02a042b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1294&q=80");
+        routeMap.setImage(placeholderImage);
+        routeMap.setSmooth(true);
+        routeMap.setCache(true);
+    }
+
 }
