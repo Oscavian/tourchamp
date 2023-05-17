8108379246c15d28f7db9d421a946a64f9c0f418
Oscavian <dev@oscavian.com>
2023-05-17 12:00:13 +0200

    Add TourLogic Method signatures.

--- src/main/java/at/fhtw/bif/swen/businesslogic/ITourLogic.java
+++ src/main/java/at/fhtw/bif/swen/businesslogic/ITourLogic.java
@@ -2,10 +2,37 @@ package at.fhtw.bif.swen.businesslogic;
 
 import at.fhtw.bif.swen.dto.TourDTO;
 
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
 public interface ITourLogic {
 
-    default TourDTO saveTour() {
-        return null;
-    }
+    void saveTour(TourDTO tourDTO);
+
+    void updateTour(TourDTO tourDTO);
+
+    void deleteTour(TourDTO tourDTO);
+
+    //fetch TourListItems
+    List<TourDTO> getAll();
+
+    TourDTO getTourById(Integer id);
+
+    List<TourDTO> search(String searchString);
+
+    void importTours(File tourFile);
+
+    //export into application folder exports/
+    void exportTours();
+
+
+
+
+
+
+
+
 
 }
