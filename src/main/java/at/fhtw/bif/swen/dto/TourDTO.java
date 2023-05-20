a2cf266ee1efae26d9ce0f0bf576883a4b9cb17c
Oscavian <dev@oscavian.com>
2023-05-20 15:21:46 +0200

    Change DTOs from records to classes.

--- src/main/java/at/fhtw/bif/swen/dto/TourDTO.java
+++ src/main/java/at/fhtw/bif/swen/dto/TourDTO.java
@@ -1,22 +1,32 @@
 package at.fhtw.bif.swen.dto;
 
 import at.fhtw.bif.swen.util.TransportType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 import java.util.List;
 
-public record TourDTO(
-        Integer id,
-        String name,
-        String description,
-        String start,
-        String destination,
-        TransportType transportType,
-        Integer distance,
-        Date estimatedTime,
-        Integer childFriendliness,
-        Integer popularity,
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TourDTO {
+    Integer id;
+    String name;
+    String description;
+    String start;
+    String destination;
+    TransportType transportType;
+    Integer distance;
+    Date estimatedTime;
+    Integer childFriendliness;
+    Integer popularity;
+    String mapURL;
+    List<TourLogDTO> logs;
 
-        String mapURL, //dunno how to tra
-        List<TourLogDTO> logs
 
-) {}
+
+}
--- src/main/java/at/fhtw/bif/swen/dto/TourLogDTO.java
+++ src/main/java/at/fhtw/bif/swen/dto/TourLogDTO.java
@@ -1,16 +1,24 @@
 package at.fhtw.bif.swen.dto;
 
 import at.fhtw.bif.swen.util.Difficulty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.Duration;
 import java.util.Date;
 
-public record TourLogDTO(
-        Integer id,
-        Integer tour_id,
-        Date timestamp,
-        String comment,
-        Difficulty difficulty,
-        Duration totalTime,
-        Integer rating
-) {}
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TourLogDTO {
+    Integer id;
+    Integer tour_id;
+    Date timestamp;
+    String comment;
+    Difficulty difficulty;
+    Duration totalTime;
+    Integer rating;
+}
